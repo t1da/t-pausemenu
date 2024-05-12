21289b22e649878faacb7507a39f478269bd1d92
toda <137654700+t1da@users.noreply.github.com>
2024-05-12 22:39:56 +0200

    Update App.svelte

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -4,7 +4,8 @@
   import { SendNUI } from "./utils/SendNUI";
   import { Locale } from './store/stores';
   import { scale } from "svelte/transition";
-  import { IconSettings, IconLogout, IconMap } from "@tabler/icons-svelte";
+  import IconSettings from "./lib/icons/IconSettings.svelte";
+  import IconDisconnect from "./lib/icons/IconDisconnect.svelte";
   import Disconnect from './lib/Disconnect.svelte';
 
   let openUI: boolean = false;
@@ -58,18 +59,18 @@
         <div class="flex flex-1">
           <div class="w-[90vh] mr-4">
             <button on:click={openMap} class="bg-neutral-800 rounded-lg flex flex-col items-center justify-center h-full w-full relative overflow-hidden transition-all duration-300 hover:bg-neutral-900">
-              <img src="assets/map.png" alt="Map" class="h-[70vh] w-[90vh] opacity-20 rounded-lg transition-transform duration-300 transform hover:scale-105"/>
+              <img src="assets/map/map.png" alt="Map" class="h-[70vh] w-[90vh] opacity-20 rounded-lg transition-transform duration-300 transform hover:scale-105"/>
             </button>
           </div>  
           <div class="flex-1 flex flex-col justify-center">
             <button on:click={openSettings} class="bg-neutral-800 opacity-80 rounded-lg flex flex-col items-center justify-center h-full relative transition-all duration-300 hover:bg-neutral-900">
-              <IconSettings size={75} class="transform hover:scale-110 transition-all duration-300" />
+              <IconSettings />
               <span class="text-xl font-bold uppercase mt-2 opacity-80">{$Locale.settings}</span>
               <div class="absolute bottom-0 w-full h-full rounded-full bg-neutral-500 opacity-20 filter blur-[50px] transition-all duration-300 pointer-events-none"></div>
             </button>
                         
             <button on:click={disconnect} class="bg-neutral-800 opacity-80 rounded-lg flex flex-col items-center justify-center h-full mt-4 relative transition-all duration-300 hover:bg-neutral-900">
-              <IconLogout size={75} class="transform hover:scale-110 transition-all duration-300" />
+              <IconDisconnect />
               <span class="text-xl font-bold uppercase mt-2 opacity-80">{$Locale.disconnect}</span>
               <div class="absolute bottom-0 w-full h-full rounded-full bg-neutral-500 opacity-20 filter blur-[50px] transition-all duration-300 pointer-events-none"></div>
             </button>
