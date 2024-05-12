2eb84b287a73bc6ed025c94008e6342f16a7913b
toda <137654700+t1da@users.noreply.github.com>
2024-05-12 22:39:26 +0200

    Add files via upload

---
+++ web/src/lib/icons/IconDisconnect.svelte
@@ -0,0 +1,8 @@
+<main class="transform hover:scale-110 transition-all duration-300">
+    <svg  xmlns="http://www.w3.org/2000/svg"  width="75"  height="75"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-logout">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+        <path d="M9 12h12l-3 -3" />
+        <path d="M18 15l3 -3" />
+    </svg>
+</main>
